5e620f6818e0dfd3574681b55a6cf8e264ccdd8e
Simonek, Jan <jsimonek@netsuite.com>
2021-03-31 19:33:52 +0200

    Fixing compilation

--- keyboards/ergodox_ez/rules.mk
+++ keyboards/ergodox_ez/rules.mk
@@ -9,7 +9,7 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = halfkay
+BOOTLOADER = bootloadHID
 
 # If you have Left LEDs (see
 # https://geekhack.org/index.php?topic=22780.msg873819#msg873819 for
@@ -24,7 +24,7 @@ MOUSEKEY_ENABLE  = yes  # Mouse keys
 EXTRAKEY_ENABLE  = yes  # Audio control and System control
 CONSOLE_ENABLE   = no   # Console for debug
 COMMAND_ENABLE   = yes  # Commands for debug and configuration
-CUSTOM_MATRIX    = yes  # Custom matrix file for the ErgoDox EZ
+CUSTOM_MATRIX    = lite # Custom matrix file for the ErgoDox EZ
 NKRO_ENABLE      = yes  # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 UNICODE_ENABLE   = yes  # Unicode
 SWAP_HANDS_ENABLE= yes  # Allow swapping hands of keyboard
